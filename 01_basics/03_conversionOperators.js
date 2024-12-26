a16faf7b3acdfb017d24f203b4419e17759756d7
DsDeepti30 <dsdeepti71@gmail.com>
2024-12-26 21:34:05 +0530

    Create 03_conversionOperators.js

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,54 @@
+let score = "33a"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=>33
+//"33ac"=>Nan
+//tre=>1 false=>
+
+    let isLoggedIn = 1
+    let booleanIsLoggedIn = Boolean(isLoggedIn)
+    console.log(booleanIsLoggedIn);
+
+    //1=>true 0=>false
+    //""=>false
+    //"hello"=>true
+
+    let someNumber = 33
+    let stringNumber = String(someNumber)
+    console.log(stringNumber);
+    console.log(typeof stringNumber);
+
+    // ************Operations*********************
+
+    let value = 3
+    let negVale = -value
+    console.log(negVale);
+
+    console.log(2+2);
+    console.log(2-2);
+    console.log(2*2);
+    console.log(2/2); 
+    console.log(2%2);
+
+    let str1 = "hello"
+    let str2 = "bye"
+
+    let str3 = str1+ str2
+    console.log(str3);
+
+    console.log("1" +2);
+    console.log(1+"2");
+    console.log("1"+2+2);
+    console.log(1+2+"2");
+
+    console.log(true);   // true
+    console.log(+true);  //1
+   // console.log(true+); // error
+
+
+   
