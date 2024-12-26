60b746df5cfa7dc0ac206548f8a6c586ee6f1c5a
DsDeepti30 <dsdeepti71@gmail.com>
2024-12-26 21:37:06 +0530

    Create 02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3+3) // we are using nodes, not browser
+
+console.log(3+3)  // code readability should be high
+console.log("hello")
+
+let name = "xyz"
+let age = 12
+let isLoggedIn = false
+
+// number => 2 to ower 53
+// bigint
+// string=> ""
+// boolean => true/false
+//null => standalone value
+// undefined => 
+    // symbol => unique
+
+//object
+
+console.log(typeof "hello");
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
